64c44db313d5b7ada1dcffcbdacd6bbfaa09947b
Diala Ayoub <112423367+dialaay5@users.noreply.github.com>
2023-02-06 18:58:06 +0200

    Update Main.java

--- src/com/example/Main.java
+++ src/com/example/Main.java
@@ -3,7 +3,7 @@ package com.example;
 public class Main {
     public static void main(String[] args) {
         try {
-            JavaExceptions.checkJopTitle("Fullstack");
+            JavaExceptions.checkJopTitle("fullstack");
         }
         catch (IllegalArgumentException e){
             e.printStackTrace();
